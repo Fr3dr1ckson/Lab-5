0328cba5c0eea90484c0b2268e695a14f9c7765f
yablonya <122230172+yablonya@users.noreply.github.com>
2023-10-20 02:01:56 +0300

    sucked so hard that i can't stand on my legs anymore

--- js/script.js
+++ js/script.js
@@ -1,44 +1,63 @@
 function showError(element, message) {
-    element.classList.toggle('errorText', !!message);
+    element.classList.toggle('errorMassage', !!message);
     element.innerText = message || '';
 }
 
-function handleNegNums(width, length) {
-    let errorWidthText = document.getElementById('errorWidthMessage');
-    let errorLengthText = document.getElementById('errorLengthMessage');
-    let flag = false;
-    if (length < 0)
-    {
-        showError(errorLengthText, 'Число має бути більше 0');
-        flag = true;
-    }
-    else if (isNaN(length) || length>=0)
-    {
-        showError(errorLengthText, '');
-    }
-
-    if (width < 0)
-    {
-        showError(errorWidthText, 'Число має бути більше 0');
-        flag = true;
-    }
-    else if (isNaN(width) || width>=0)
-    {
-        showError(errorWidthText, '');
-    }
-    return flag;
-}
 function calculate() {
-    let width = document.getElementById('width').value;
-    let length = document.getElementById('length').value;
-    if(handleNegNums(width,length)){
-        return
-    }
-    let area = width*length;
-    let perimeter = width*2+length*2;
-    let diagonal = Math.sqrt(width*width+length*length)
+    let width = parseInt(document.getElementById('width').value);
+    let length = parseInt(document.getElementById('length').value);
+    if(
+        handleIncorrectValue(length, 'errorLengthMassage', 0)
+        |
+        handleIncorrectValue(width, 'errorWidthMassage', 0)
+    ) { return; }
+
+    let area = width * length;
+    let perimeter = (width + length) * 2;
+    let diagonal = Math.sqrt(width**2 + length**2)
 
     document.getElementById('areaOutput').innerText = area;
     document.getElementById('perimeterOutput').innerText = perimeter;
     document.getElementById('diagonalOutput').innerText = diagonal;
+}
+
+function calcDayOfWeek() {
+    let year = parseInt(document.getElementById('year').value);
+    let month = parseInt(document.getElementById('month').value);
+    let day = parseInt(document.getElementById('day').value);
+
+    if(
+        handleIncorrectValue(year, 'errorYearMassage', 1999, 2023)
+        |
+        handleIncorrectValue(month, 'errorMonthMassage', 1, 12)
+        |
+        handleIncorrectValue(day, 'errorDayMassage', 1, 31)
+    ) { return; }
+
+    if (month < 3) {
+        month += 12;
+        year--;
+    }
+
+    let k = year % 100;
+    let j = Math.floor(year / 100);
+
+    let dayOfWeek = (day + Math.floor(13 * (month + 1) / 5) + k + Math.floor(k / 4) + Math.floor(j / 4) - 2 * j) % 7;
+    let week = ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П\'ятниця', 'Субота']
+    document.getElementById('dayOfWeek').innerText = week[(dayOfWeek + 6) % 7];
+}
+
+function handleIncorrectValue(value, errorMassageId, minInclude, maxInclude=Infinity) {
+    let errorMassage = document.getElementById(errorMassageId);
+
+    if (value < minInclude || value > maxInclude) {
+        showError(errorMassage, `Число має бути в від ${minInclude} до ${maxInclude}`);
+        return true;
+
+    } else if(isNaN(value)) {
+        showError(errorMassage, '');
+        return true;
+    }
+    showError(errorMassage, '');
+    return false;
 }
